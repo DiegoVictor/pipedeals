676112a40af004eec49f37d902c415914881c5a1
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-20 23:06:07 -0300

    Configure eslint, prettier and nodemon

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+  },
+};
