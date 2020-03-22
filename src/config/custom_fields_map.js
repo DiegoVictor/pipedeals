751eb6d06923319e11ba9e22e5809b859d016c6a
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-22 11:19:30 -0300

    Map custom fields

---
+++ src/config/custom_fields_map.js
@@ -0,0 +1,5 @@
+export const supplier_name = '6866136a4bc7b12a75897df3d7ae168b46497b10';
+
+export const payment_method_id = '4275aa493fbf4aeeefb0d918cd90df6273655368';
+
+export const parcels_total = '6427f011f186d62449eb8caf53edb2a52cf959a4';
