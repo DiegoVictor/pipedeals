6b74b476731941c904810e1a0009d10919a8c0be
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-23 18:22:58 -0300

    Send deal to bling

--- src/app/controllers/PipedriveEventController.js
+++ src/app/controllers/PipedriveEventController.js
@@ -16,58 +16,55 @@ class PipedriveEventController {
       case 'updated.deal': {
         if (current && current.status === 'won') {
           try {
-            const { data: deals } = await Pipedrive.get('deals', {
+            const { data: deal } = await Pipedrive.get(`/deals/${current.id}`, {
               params: {
-                status: 'won',
                 api_token: process.env.PIPEDRIVE_API_TOKEN,
               },
             });
 
-            deals.data.forEach(async deal => {
-              const { id, person_id } = deal;
+            const { id, person_id } = deal.data;
 
-              const items = [];
-              const { data: products } = await Pipedrive.get(
-                `/deals/${id}/products`,
-                {
-                  params: { api_token: process.env.PIPEDRIVE_API_TOKEN },
-                }
-              );
-
-              products.data.forEach(({ name, quantity, item_price }) => {
-                items.push({
-                  quantity,
-                  description: name,
-                  unitary_value: item_price,
-                });
-              });
-
-              const amount = items.reduce(
-                (sum, item) => item.unitary_value * item.quantity + sum,
-                0
-              );
-
-              const parcels = [];
-              for (let i = 1; i <= deal[parcels_total]; i += 1) {
-                parcels.push({
-                  payment_term_in_days: 30 * i,
-                  value: amount / deal[parcels_total],
-                });
+            const items = [];
+            const { data: products } = await Pipedrive.get(
+              `/deals/${id}/products`,
+              {
+                params: { api_token: process.env.PIPEDRIVE_API_TOKEN },
               }
+            );
 
-              await Opportunity.create({
-                amount,
-                supplier: {
-                  name: deal[supplier_name],
-                },
-                client: {
-                  pipedrive_id: id,
-                  name: person_id.name,
-                },
-                payment_method_id: parseInt(deal[payment_method_id], 10),
-                parcels,
-                items,
+            products.data.forEach(({ name, quantity, item_price }) => {
+              items.push({
+                quantity,
+                description: name,
+                unitary_value: item_price,
               });
+            });
+
+            const amount = items.reduce(
+              (sum, item) => item.unitary_value * item.quantity + sum,
+              0
+            );
+
+            const parcels = [];
+            for (let i = 1; i <= deal.data[parcels_total]; i += 1) {
+              parcels.push({
+                payment_term_in_days: 30 * i,
+                value: amount / deal.data[parcels_total],
+              });
+            }
+
+            await Opportunity.create({
+              amount,
+              supplier: {
+                name: deal.data[supplier_name],
+              },
+              client: {
+                pipedrive_id: id,
+                name: person_id.name,
+              },
+              payment_method_id: parseInt(deal.data[payment_method_id], 10),
+              parcels,
+              items,
             });
           } catch (err) {
             Sentry.captureException(err);
@@ -81,8 +78,6 @@ class PipedriveEventController {
         }
         break;
       }
-
-      default:
     }
 
     return res.send({
