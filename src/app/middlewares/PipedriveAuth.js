2b764c98152844ca446c8cdf51e954bb1e25639d
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-23 18:23:14 -0300

    Prevent error when token auth is not present

--- src/app/middlewares/PipedriveAuth.js
+++ src/app/middlewares/PipedriveAuth.js
@@ -2,6 +2,15 @@ import atob from 'atob';
 
 export default (req, res, next) => {
   const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(400).json({
+      error: {
+        message: 'Missing authorization',
+      },
+    });
+  }
+
   const [user, pass] = atob(authorization.split(' ').pop()).split(':');
 
   if (
