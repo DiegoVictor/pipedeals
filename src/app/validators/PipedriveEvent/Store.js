c8588f33ef5fb8bcbfae856a08a222ec461105fa
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-04-22 17:52:57 -0300

    Validate events from pipedrive

--- src/app/validators/PipedriveEvent/Store.js
+++ src/app/validators/PipedriveEvent/Store.js
@@ -1,11 +1,13 @@
 import { celebrate, Segments, Joi } from 'celebrate';
 
 export default celebrate({
-  [Segments.BODY]: Joi.object().keys({
+  [Segments.BODY]: Joi.object({
     event: Joi.string().required(),
     current: Joi.object({
       id: Joi.number().required(),
       status: Joi.string().required(),
-    }).required(),
-  }),
+    })
+      .unknown()
+      .required(),
+  }).unknown(),
 });
