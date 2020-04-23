b794ac1cf9be5e95cacdc6a70df73a46bc3f8b26
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-04-23 19:01:46 -0300

    Configure jest and install test dependencies

--- jest.config.js
+++ jest.config.js
@@ -136,7 +136,7 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ['**/tests/**/*.test.js'],
+  testMatch: ['**/tests/**/*.spec.js'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
