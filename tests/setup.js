525b1acbd3d49a61459988d888169c9a28066bea
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-07-20 08:38:37 -0300

    Update memory database initialization

--- tests/setup.js
+++ tests/setup.js
@@ -1,16 +1,10 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 
-const mongod = new MongoMemoryServer({
-  instance: {
-    ip: '127.0.0.1',
-    dbName: 'jest',
-    port: 27018,
-  },
-  autoStart: false,
-  debug: false,
-});
+const mongod = new MongoMemoryServer();
 
 module.exports = async () => {
-  process.env.MONGO_URL = await mongod.getConnectionString();
+  await mongod.start();
+
+  process.env.MONGO_URL = mongod.getUri();
   global.__MONGOD__ = mongod;
 };
